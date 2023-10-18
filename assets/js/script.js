9cacf7acdfdfdd1f75cbbe137e5269bd797e0ec1
nkartikaxioned <kartikn@axioned.com>
2023-10-18 17:50:41 +0530

    modify

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const search = document.querySelector('.input-text'),
   date = document.querySelector('.date'),
   cityy = document.querySelector('.city'),
   temprature = document.querySelector('.temprature'),
-  tempSymbol = document.querySelector('.temp-symbol'),
+  figure=document.querySelector('figure'),
   rainProbability = document.querySelector('.rain-probability'),
   windSpeed = document.querySelector('.wind-speed'),
   windDirection = document.querySelector('.wind-direction'),
@@ -20,32 +20,26 @@ const now = new Date();
 async function getWeather(city = 'mumbai') {
   try {
     var res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c33ecddffd96e7dc6aa94c5e89e44aa1&units=metric`);
-    if(!res) {
-         throw new Error('Failed to fetch weather data')
+    console.log(res)
+    if (!res) {
+      throw new Error('Failed to fetch weather data')
     } else {
       var data = await res.json();
+      console.log(data)
       cityy.innerText = data.name;
-      temprature.innerText = Math.round(data.main.temp) + ' C';
+      temprature.innerText = Math.round(data.main.temp) + ' °C';
       windSpeed.innerText = data.wind.speed + ' Km/hr';
+      figure.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Symbol"></img>`;
       windDirection.innerText = degreesToDirection(data.wind.deg);
       day.innerText = days[now.getDay()];
       date.innerText = now.getDate() + " " + monthNames[now.getMonth()];
     }
-   
-  } catch (error) {
-    dataContainer.innerHTML = `<p class="error-msg">Enter Valid City Name.</p>`;
-    submitButton.addEventListener('click', (e) => {
-      e.preventDefault();
-      if (search.value === '' || search.value === null) {
-        dataContainer.innerHTML = `<p class="error-msg">City Name Cannot Be Empty.</p>`;
-      } else {
-        getWeather(search.value);
-      }
-    })
-  }
- 
-}
 
+  } catch (error) {
+    alert("enter valid city name")
+  }
+
+}
 
 function degreesToDirection(degrees) {
   const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
@@ -57,18 +51,16 @@ function degreesToDirection(degrees) {
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
   if (search.value === '' || search.value === null) {
-    dataContainer.innerHTML = `<p class="error-msg">City Name Cannot Be Empty.</p>`;
+    alert('City Name Cannot Be Empty.');
   } else {
     getWeather(search.value);
   }
 })
 
 window.addEventListener("load", () => {
-  const defaultCity='mumbai';
+  const defaultCity = 'mumbai';
   getWeather(defaultCity);
 });
-
-
 
 
 
